658af9491b5ce250b9bff77c8e96f8d48b895b18
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-11-16 11:41:54 -0300

    🎉 Init: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryDark: "#0E0E0E",
+        primaryBlue: "#083A67",
+        primaryGreen: "#193E1F",
+        primaryGray: "#A4A4A4",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config
